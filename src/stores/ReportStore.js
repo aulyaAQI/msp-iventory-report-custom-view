f4ca9328121828ea3fb0bd19a81258624863b028
DESKTOP-PPEVJ58\kuya <aulya.khatulistivani@gmail.com>
2024-01-31 02:39:48 +0700

    group

--- src/stores/ReportStore.js
+++ src/stores/ReportStore.js
@@ -19,22 +19,41 @@ export const useReportStore = defineStore({
       console.log(products.value, 'productStore');
       console.log(records.value, 'receivingStore');
 
-      const reportList = products.value.map((product) => {
-        const receivingRecords = records.value.filter((record) => {
-          return record.Part_No.value === product.Part_No.value;
-        });
+      const filteredRecords = this.groupReceivingByPartNoAndSumQtyReceived(records.value);
+      console.log({filteredRecords});
 
-        const totalQuantity = receivingRecords.reduce((acc, record) => {
-          return acc + record.Quantity.value;
-        }, 0);
+      if (!filteredRecords.length) return {};
+
+      const reportList = products.value.map((product) => {
+        const receivingReference = filteredRecords[product.Part_No.value];
+
+        const qtyReceived = receivingReference ? receivingReference.qtyReceived : 0;
 
         return {
-          ...product,
-          totalQuantity,
+          partNo: product.Part_No.value,
+          // description: product.Description.value,
+          qtyReceived,
         };
       });
 
+      console.log({reportList});
+
       this.reportList = reportList;
+    },
+    groupReceivingByPartNoAndSumQtyReceived(records) {
+      const filteredRecords = records.reduce((acc, record) => {
+        // const {partNo, qtyReceived} = record;
+        const partNo = record.Part_No_0.value;
+        const qtyReceived = parseFloat(record.Qty_Received_0.value);
+
+        if (!acc[partNo]) {
+          acc[partNo] = {partNo, qtyReceived};
+        } else {
+          acc[partNo].qtyReceived += qtyReceived;
+        }
+        return acc;
+      }, {});
+      return Object.values(filteredRecords);
     },
   },
   getters: {},
